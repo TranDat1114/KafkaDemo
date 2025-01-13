875d54b3a76c25d2bb74e26a858162052fb037eb
trandat1114 <cctalk1114@gmail.com>
2025-01-13 10:05:02 +0700

    first commit

---
+++ Consumer/ConsumerKafka.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+class KafkaConsumer
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            GroupId = "test-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe("my-topic");
+
+        CancellationTokenSource cts = new();
+        Console.CancelKeyPress += (_, e) =>
+        {
+            e.Cancel = true;
+            cts.Cancel();
+        };
+
+        try
+        {
+            while (true)
+            {
+                var cr = consumer.Consume(cts.Token);
+                Console.WriteLine($"Nhận được thông điệp: {cr.Value}");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+}
---
+++ Producer/ProducerKafka.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Confluent.Kafka;
+
+class KafkaProducer
+{
+    public static void Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+        Console.WriteLine("Nhập thông điệp (gõ 'exit' để thoát):");
+        string message;
+        while ((message = Console.ReadLine()) != "exit")
+        {
+            producer.Produce("my-topic", new Message<Null, string> { Value = message });
+            Console.WriteLine($"Đã gửi: {message}");
+        }
+
+        producer.Flush(TimeSpan.FromSeconds(10));
+    }
+}
